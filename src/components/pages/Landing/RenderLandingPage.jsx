5339b1c76f84fd8fb0a3e5108a1648e152840c24
Kris Thornton <Kristhornton@ymail.com>
2023-11-20 22:27:10 -0800

    I have started working on the bottom section design.

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -98,6 +98,33 @@ function RenderLandingPage(props) {
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
         {/* <div className="bottom-section">*/}
+        <div className="bottom-title">
+          <h1>Systemic Disparity Insights</h1>
+        </div>
+
+        <div className="bottom-section-header">
+          <h1>36%</h1>
+          <h1>5%</h1>
+          <h1>6x Lower</h1>
+        </div>
+
+        <div className="bottom-section">
+          <p>
+            By the end of the Trump administration, the average asylum office
+            grant rate had fallen 36 percent from an average of 44 percent in
+            fiscal year 2016 to 28 percent in fiscal year 2020.
+          </p>
+          <p>
+            The New York asylum office grant rate dropped to 5 percent in fiscal
+            year 2020.
+          </p>
+          <p>
+            Between fiscal year 2017 and 2020, the New York asylum office's
+            average grant rate was six times lower than the San Francisco asylum
+            office.
+          </p>
+        </div>
+
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
