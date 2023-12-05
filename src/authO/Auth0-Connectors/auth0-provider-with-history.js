2c15b3674e9626846e1c476e7d84e178d99f440d
Kris Thornton <Kristhornton@ymail.com>
2023-12-04 20:03:01 -0800

    updated the redirectUri . Project is now working with Auth0

--- src/authO/Auth0-Connectors/auth0-provider-with-history.js
+++ src/authO/Auth0-Connectors/auth0-provider-with-history.js
@@ -1,27 +1,27 @@
 import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
-// import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 export const Auth0ProviderWithHistory = ({ children }) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-  const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
-  // const history = useHistory();
+  const history = useHistory();
 
-  // const onRedirectCallback = appState => {
-  //   history.push(
-  //     appState && appState.targetUrl
-  //       ? appState.targetUrl
-  //       : window.location.href = "http://localhost:3000/profile"
-  //   );
-  // };
+  const onRedirectCallback = appState => {
+    history.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : (window.location.href = 'http://localhost:3000/profile')
+    );
+  };
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirect_uri={redirectUri}
-      // onRedirectCallback={onRedirectCallback}
+      redirectUri={window.location.href}
+      // updated the redirectUri from {redirectUri} to what you see above.
+      onRedirectCallback={onRedirectCallback}
     >
       {children}
     </Auth0Provider>
